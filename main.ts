2e89a1fc5eed21a7df6a4e5a43c71995581d2d95
Knob <59198478+nbr41to@users.noreply.github.com>
2021-02-17 14:26:23 +0900

    競合の回避

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 // console.log("double")
-console.log('double')
+console.log('double');
 
 type Book = {
-  id: number
-  name: string
-}
+  id: number;
+  name: string;
+};
 
 const book = ({ id, name }: Book) => {
-  document.body.textContent = 'ID: ' + id + ', title: ' + name
-}
+  document.body.textContent = 'ID: ' + id + ', title: ' + name;
+};
 const myBook = {
   id: 123,
   name: 'programming',
-}
+};
 
-book(myBook)
+book(myBook);
