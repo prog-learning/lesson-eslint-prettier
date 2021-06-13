97dfd6519771f08169a53f60a16a260871743dcb
Nobuyuki Kobayashi <nbr.41to@gmail.com>
2021-06-13 14:20:40 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 export const App: React.FC = () => {
-  console.log('aaa');
-  const [state, setState] = React.useState<string>();
-  return <Styled>hello</Styled>;
-};
+  console.log('aaa')
+  const [state, setState] = React.useState<string>()
+  return <Styled>hello</Styled>
+}
 
 const Styled = styled.h1`
   color: tomato;
-`;
+`
